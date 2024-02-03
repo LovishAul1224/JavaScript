8b52d5905b575f23d8708b4a824aa3b71d3adcb9
LovishAul1224 <138647080+LovishAul1224@users.noreply.github.com>
2024-02-03 18:59:19 +0000

    datatypes

---
+++ new/datatype.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as a newer version
+//alert( 2 + 2 ); // we are using nodejs not browser
+// code readability should be high
+// number => 2 raise to the power 53
+// bigint
+// string = ""
+// boolean
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(undefined); // undefined
+console.log(null); // object
+
