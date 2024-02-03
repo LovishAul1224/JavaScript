197d7efa588575788c0918259ab03012aadd47c9
LovishAul1224 <138647080+LovishAul1224@users.noreply.github.com>
2024-02-03 19:00:01 +0000

    variables

---
+++ new/variables.js
@@ -0,0 +1,11 @@
+const userId = 23456
+let accountemail = "Lovish@google.com"
+var password = "1234"
+accountCity = "Yamunanagar"
+// warning value of const cannot be re-declared or declared again
+console.log(accountCity);
+console.table([ userId, accountemail , accountCity , password])
+/*
+prefer not to use var
+because of issues in block scope and functional scope
+*/
