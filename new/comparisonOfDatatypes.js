05f550ca75309a2f5a83f9e98373ba33bc41f2b0
LovishAul1224 <138647080+LovishAul1224@users.noreply.github.com>
2024-02-04 17:33:26 +0000

    comparison of Datatypes

---
+++ new/comparisonOfDatatypes.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+
+console.log(null > 0 );
+console.log(null == 0 );
+console.log(null >= 0 );
+
+// we should avoid  these types of conversions
+// Clean code should be your first priority
