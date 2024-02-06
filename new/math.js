a9abf98bbede5f215758144664ec9126e79fcdf1
LovishAul1224 <138647080+LovishAul1224@users.noreply.github.com>
2024-02-06 16:42:33 +0000

    some mathematical calculations

---
+++ new/math.js
@@ -0,0 +1,16 @@
+// ++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++
+console.log(Math);  // it is a type of object, if you want detailed information run this function on console in browser.
+console.log(Math.abs(-100)); // used to convert negative values to positive only.
+// round is used to round off the value
+console.log(Math.round(34.3));
+console.log(Math.round(34.5));
+// ceil is to print top upper value
+console.log(Math.ceil(4.2));
+console.log(Math.round(4.2));
+// min and max are used to find highest and lowest value in the ArrayList.
+console.log(" ");
+console.log(Math.random()); // it gives random value between 0 and 1 
+console.log((Math.random()*10) + 1);
+const min = 10 
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min ); 
