5a30a7e620d023c45722f19d6c27d27667cfaa72
LovishAul1224 <138647080+LovishAul1224@users.noreply.github.com>
2024-02-06 16:17:28 +0000

    learned some intresting thing to print number in different types in JavaScript

---
+++ new/numberAndMath.js
@@ -0,0 +1,10 @@
+const score = 500 
+const balance = new Number(1000)
+console.log(balance); 
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+// to precision is used to make precise value before the decimal places
+
+const cost = 1000000
+console.log(cost.toLocaleString('en-IN')); // to localeString is used to use commas in Large numeric values
