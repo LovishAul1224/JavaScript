7fda2e549e09a2f35957ce9c20b22dc007f0146e
LovishAul1224 <138647080+LovishAul1224@users.noreply.github.com>
2024-02-07 19:42:43 +0000

    Learned to print date and time using different keywords

---
+++ new/dateAndTime.js
@@ -0,0 +1,10 @@
+// to fetch date and time date function is used
+let Mydate = new Date()
+console.log(Mydate.toDateString());
+console.log(Mydate.toISOString());
+console.log(Mydate.toJSON);
+console.log(Mydate.toLocaleDateString());
+console.log(Mydate.toLocaleTimeString());
+console.log(Mydate.toLocaleString());
+// type of date is object
+console.log(typeof Mydate);
